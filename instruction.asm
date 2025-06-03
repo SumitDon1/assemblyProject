65e2c8a123627196b4a322157f6f846d55da76e4
MonkeyMan <110273989+SumitDon1@users.noreply.github.com>
2025-06-03 18:47:32 +0530

    Create instruction.asm

---
+++ instruction.asm
@@ -0,0 +1,6 @@
+; addressing mode
+;;;IMMEDIATE MODE
+LDA #80                 ; load A register with literal decimal 80
+
+;;;AbSOLUTE (zero page mode) 
+LDA $80                 ; loads A register with the value inside memory address $80
