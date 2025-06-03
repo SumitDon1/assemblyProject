4a1c391c51da67ebaa26e83a1bc62251cea0e5a4
MonkeyMan <110273989+SumitDon1@users.noreply.github.com>
2025-06-03 18:52:34 +0530

    Update instruction.asm

--- instruction.asm
+++ instruction.asm
@@ -4,3 +4,8 @@ LDA #80                 ; load A register with literal decimal 80
 
 ;;;AbSOLUTE (zero page mode) 
 LDA $80                 ; loads A register with the value inside memory address $80
+
+;;;ABSOLUTE (zero page indexed)
+STA $80,X               ; store the value of A into memory address {$80 + X}
+
+; Page zero are the memory address bw $00 and $FF
